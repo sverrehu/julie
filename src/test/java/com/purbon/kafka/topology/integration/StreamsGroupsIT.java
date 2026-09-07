fbbe5ff398d6ab7f9b7b3e1df248a58c33d5635a
Rune Langleite <rune.langleite@statnett.no>
2026-09-07 13:13:36 +0200

    chore: update expected values in tests

--- src/test/java/com/purbon/kafka/topology/integration/StreamsGroupsIT.java
+++ src/test/java/com/purbon/kafka/topology/integration/StreamsGroupsIT.java
@@ -19,15 +19,6 @@ import org.junit.Test;
 
 public final class StreamsGroupsIT {
 
-  public static final String TOPIC_A = "topic-A";
-  public static final String STREAMS_APP_ID = "streams-appid";
-
-  public static final String HEARTBEAT_INTERVAL_MS = "6000";
-  public static final String NUM_STANDBY_REPLICAS = "2";
-  public static final String SESSION_TIMEOUT_MS = "60000";
-  public static final String INITIAL_REBALANCE_MS = "2000";
-  private static final String CONSUMER_GROUP = "streams-appid";
-
   private static SaslPlaintextKafkaContainer container;
   private AdminClient adminClient;
 
@@ -80,10 +71,10 @@ public final class StreamsGroupsIT {
                 System.out.println(entry.name() + " : " + entry.value());
               });
       Config config = result.get(groupResource);
-      assertEquals(SESSION_TIMEOUT_MS, config.get("streams.session.timeout.ms").value());
-      assertEquals(HEARTBEAT_INTERVAL_MS, config.get("streams.heartbeat.interval.ms").value());
-      assertEquals(NUM_STANDBY_REPLICAS, config.get("streams.num.standby.replicas").value());
-      assertEquals(INITIAL_REBALANCE_MS, config.get("streams.initial.rebalance.delay.ms").value());
+      assertEquals("60000", config.get("streams.session.timeout.ms").value());
+      assertEquals("6000", config.get("streams.heartbeat.interval.ms").value());
+      assertEquals("1", config.get("streams.num.standby.replicas").value());
+      assertEquals("2000", config.get("streams.initial.rebalance.delay.ms").value());
     } catch (InterruptedException | ExecutionException e) {
       throw new RuntimeException(e);
     }
@@ -106,7 +97,7 @@ public final class StreamsGroupsIT {
       Config config = result.get(groupResource);
       assertEquals("50000", config.get("streams.session.timeout.ms").value());
       assertEquals("5000", config.get("streams.heartbeat.interval.ms").value());
-      assertEquals("6", config.get("streams.num.standby.replicas").value());
+      assertEquals("2", config.get("streams.num.standby.replicas").value());
       assertEquals("3000", config.get("streams.initial.rebalance.delay.ms").value());
     } catch (InterruptedException | ExecutionException e) {
       throw new RuntimeException(e);
