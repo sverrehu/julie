353c1f7b2bea102254cd2905eb605bc2c38152f4
Rune Langleite <rune.langleite@statnett.no>
2026-09-07 12:50:16 +0200

    fix: revert to clientside streams config, set replicas to brokerside max

--- src/main/java/com/purbon/kafka/topology/api/adminclient/TopologyBuilderAdminClient.java
+++ src/main/java/com/purbon/kafka/topology/api/adminclient/TopologyBuilderAdminClient.java
@@ -304,7 +304,7 @@ public class TopologyBuilderAdminClient {
                 OpType.SET),
             new AlterConfigOp(
                 new ConfigEntry(
-                    "group.streams.initial.rebalance.delay.ms",
+                    "streams.initial.rebalance.delay.ms",
                     groupConfig
                         .getInitialRebalanceDelayMs()
                         .orElse(GroupConfig.DEFAULT_INITIAL_REBALANCE_MS)
